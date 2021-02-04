71740366b9a06dcbf850cddcbde6e85b4231329f
Kira-Mironova <kiracabezas@gmail.com>
2021-02-05 00:03:54 +0100

    otro update

---
+++ TestShopingCart/src/test/java/pruebasJUnit/ProductTest.java
@@ -0,0 +1,39 @@
+package pruebasJUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProductTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
